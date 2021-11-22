1514e5100152cfb68c6af2d0c8a713bad8b9bbef
Rubeencito <ruben.huertas@institutvidreres.cat>
2021-11-22 20:28:09 +0100

    EX7

--- jsRubenHuertas.js
+++ jsRubenHuertas.js
@@ -1,11 +1,8 @@
-//EX6
+//EX7
 
-const BigInt = 5678;
-var num = 1234;
-console.log(num + BigInt)
-console.log(num - BigInt)
-console.log(num * BigInt)
-console.log(num / BigInt)
+var catet1 = 4
+var catet2 = 4
+console.log(catet1*catet1 + catet2 *catet2)
 
 
 
