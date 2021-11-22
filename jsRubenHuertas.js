98695833cc4e5cd11d7596f5bbd33690638f7e15
Rubeencito <ruben.huertas@institutvidreres.cat>
2021-11-22 08:34:05 +0100

    EX2

--- jsRubenHuertas.js
+++ jsRubenHuertas.js
@@ -1,9 +1,11 @@
-//EX1 
+//EX2
 
-console.log('Nom: Ruben')
-console.info('Cognom1: Huertas')
-console.warn('Cognom2: Gonzalez')
-console.error('DataNaixament: 09/06/2003')
+var variable1 = 'Hola'
+let variable2 = 'Bones'
+const variable3 = 'Hello'
+console.log(variable1)
+console.log(variable2)
+console.log(variable3)
 
 
 
